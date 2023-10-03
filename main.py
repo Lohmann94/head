b6733940a459909d4051e03a162736a63e6384c0
Lohmann94 <61236600+Lohmann94@users.noreply.github.com>
2023-10-03 21:42:31 +0200

    added validation and test split capability

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from models.f_cut import CosineCutoff
 from models.models import PAINN
 from data.processed.cross_coupling import datasets
 from tqdm import tqdm
+import os
 
-data = datasets.Cross_coupling_optimized_eom2Lig()
+train_data = datasets.Cross_coupling_optimized_eom2Lig(start_index=0, end_index=5)
+val_data = datasets.Cross_coupling_optimized_eom2Lig(start_index=5, end_index=7)
+test_data = datasets.Cross_coupling_optimized_eom2Lig(start_index=7, end_index=9)
 #data = datasets.Tetris()
 
 # Check if a GPU is available
@@ -21,17 +24,39 @@ else:
 
 # Network, loss function, and optimizer
 #net = GNNInvariant(output_dim=data.num_graphs, state_dim = 5)
-net = PAINN(num_phys_dims=3, num_message_passing_rounds=5, r_cut=3)
+net = PAINN(num_phys_dims=3, num_message_passing_rounds=5, r_cut=4)
 net.to(device)
 loss_function = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+val_loss_function = torch.nn.MSELoss()
+#loss_function = torch.nn.L1Loss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.01)
 
-epochs = 10
+epochs = 100
 for epoch in range(epochs):
     optimizer.zero_grad()
-    output = net(data)
-    loss = loss_function(output, data.targets.to(device))
+    output = net(train_data)
+    loss = loss_function(output, train_data.targets.to(device))
     loss.backward()
     optimizer.step()
 
+    
+    #TODO validation loop
+
     print(f'Epoch: {epoch}, Loss: {loss}, MSE: {loss.item()}')
+'''
+optimizer.zero_grad()
+total_loss = 0
+val_output = net(val_data)
+val_loss = val_loss_function(val_output, val_data.targets.to(device))
+total_loss += val_loss
+print(f'Validation loss at epoch {epoch}: {val_loss}')
+# Compute and report the average validation loss
+average_loss = total_loss // epoch
+print(f'Average Validation Loss: {average_loss}')
+
+net.eval()
+with torch.no_grad():
+    output = net(test_data)
+    loss = loss_function(output, test_data.targets.to(device))
+
+'''
